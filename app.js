0db222579a5e38fd1a27ca3ab87d520c9a9e2f8a
Mikel Robbins <mikel@asaddu.com>
2023-04-06 16:27:54 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+$('.navItem').each(function () {
+    $(this).css('color', 'white');
+});
+
+$('#nav').css('background-color', '#145');
+
+$('li:even').each(function () {
+    $(this).css('background-color', '#ccc')
+})
+
+$('li:odd').each(function () {
+    $(this).css('background-color', '#aaa')
+})
+
+$('input').val("Mikel");
